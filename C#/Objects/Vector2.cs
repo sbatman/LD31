9efa18c7197ea3c4cd5701098aef1c3494b23825
Edwin Jones <edwinj85@gmail.com>
2014-12-06 13:32:47 +0000

    Initial commit of GameObjects
    Initial commit of GameObjects

---
+++ C#/Objects/Vector2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD31.Objects
+{
+    /// <summary>
+    /// This struct represents a two dimensional vector.
+    /// </summary>
+    struct Vector2
+    {
+        public Int32 X { get; set; }
+
+        public Int32 Y { get; set; }
+
+        public Vector2(Int32 x, Int32 y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector2(Int32 xAndY)
+        {
+            X = xAndY;
+            Y = xAndY;
+        }
+    }
+}
